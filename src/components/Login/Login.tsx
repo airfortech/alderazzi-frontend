a3596b3c7c12b3d95312ffb5880a5d8fccac71f2
airfortech <airm@xxx.yy>
2022-10-03 00:55:18 +0200

    added validation to Login

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,50 +1,83 @@
 import { UserRole } from "../../types/UserRoles";
-import { useState } from "react";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 import MenuItem from "@mui/material/MenuItem";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
+import Select from "@mui/material/Select";
 import InputLabel from "@mui/material/InputLabel";
 import FormControl from "@mui/material/FormControl";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import classes from "./Login.module.css";
 
-export const Login = () => {
-  const [role, setRole] = useState(UserRole.soldato);
+interface FormData {
+  role: string;
+  password: string;
+}
 
-  const handleChange = (event: SelectChangeEvent) => {
-    setRole(event.target.value as unknown as UserRole);
-  };
+const validationSchema = yup.object().shape({
+  role: yup.string().required(),
+  password: yup.string().required("Podaj hasło!"),
+});
+
+export const Login = () => {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors, isValid, isDirty },
+  } = useForm<FormData>({
+    mode: "onChange",
+    resolver: yupResolver(validationSchema),
+  });
+
+  const onSubmit: SubmitHandler<FormData> = data =>
+    console.log("data submitted: ", data);
 
   return (
     <div className={classes.Login}>
       <p>Zaloguj się by uzyskać uprawnienia</p>
-      <FormControl component="form" className={classes.form} fullWidth>
+      <FormControl
+        component="form"
+        className={classes.form}
+        fullWidth
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <InputLabel>Poziom uprawnień</InputLabel>
-        <Select
-          id="demo-simple-select"
-          value={role}
-          label="Poziom dostępu"
-          onChange={handleChange}
-        >
-          {Object.values(UserRole).map(userRole => (
-            <MenuItem key={userRole} value={userRole}>
-              {userRole}
-            </MenuItem>
-          ))}
-        </Select>
-        <TextField
-          id="outlined-basic"
-          label="Hasło"
-          type="password"
-          variant="outlined"
-          autoComplete="current-password"
+        <Controller
+          name="role"
+          control={control}
+          defaultValue={UserRole.soldato}
+          render={({ field: { onChange, value } }) => (
+            <Select value={value} label="Poziom uprawnień" onChange={onChange}>
+              {Object.values(UserRole).map(userRole => (
+                <MenuItem key={userRole} value={userRole}>
+                  {userRole}
+                </MenuItem>
+              ))}
+            </Select>
+          )}
         />
-        <p className={classes.error}>{"Error"}</p>
+        <Controller
+          name="password"
+          control={control}
+          defaultValue=""
+          render={({ field }) => (
+            <TextField
+              {...field}
+              label="Hasło"
+              type="password"
+              variant="outlined"
+              autoComplete="current-password"
+            />
+          )}
+        />
+        <p className={classes.error}>{errors.password?.message}</p>
         <Button
           className={classes.submit}
           type="submit"
           variant="contained"
           size="large"
+          disabled={!isValid}
         >
           Zaloguj się
         </Button>
