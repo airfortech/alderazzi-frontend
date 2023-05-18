a4a7d74363c25f70ae4ffad159f6ef008bb07ba1
airfortech <airm@xxx.yy>
2023-05-18 12:55:39 +0200

    added github actions for deploying

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,9 +2,11 @@ import axios from "axios";
 
 import { config } from "../config/config";
 
+console.log(config.axiosTimeout);
+
 export const api = axios.create({
   baseURL: config.apiUrl,
-  timeout: config.axiosTimeout || 2000,
+  timeout: config.axiosTimeout,
   withCredentials: true,
   headers: {
     "Content-Language": config.lang,
