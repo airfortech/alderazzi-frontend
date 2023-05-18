4bd219c200c06a500bde7e5c7914d8efdee2c9e4
airfortech <airm@xxx.yy>
2023-05-18 11:46:10 +0200

    added .env files to config

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,7 +4,7 @@ import { config } from "../config/config";
 
 export const api = axios.create({
   baseURL: config.apiUrl,
-  timeout: 2000,
+  timeout: config.axiosTimeout || 2000,
   withCredentials: true,
   headers: {
     "Content-Language": config.lang,
