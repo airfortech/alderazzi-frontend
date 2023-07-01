6048adc68f4b8e63f553fa9d432d080ed69b9058
airfortech <airm@xxx.yy>
2023-07-01 11:40:06 +0200

    added minutes to KeyGiverDrops table and optional reset form to Form

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,4 +2,5 @@ export const config = {
   apiUrl: process.env.REACT_APP_backend_url || "http://localhost:3001/api",
   lang: process.env.REACT_APP_language || "en",
   axiosTimeout: Number(process.env.REACT_APP_axios_timeout) || 2000,
+  keyGiverDropsRefetchIntervalInMinutes: 9,
 };
