b01a9e38038196cf26bc47716f182ac5fdf69b2f
airfortech <airm@xxx.yy>
2023-08-23 00:13:24 +0200

    added KeyGiverDropsStatsView

---
+++ src/components/KeyGiverDropsStatsInfo/KeyGiverDropsStatsInfo.tsx
@@ -0,0 +1,7 @@
+import classes from "./KeyGiverDropsStatsInfo.module.css";
+
+export const KeyGiverDropsStatsInfo = () => {
+  return (
+    <div className={classes.KeyGiverDropsStatsInfo}>KeyGiverDropsStatsInfo</div>
+  );
+};
---
+++ src/views/KeyGiverDropsStatsView/KeyGiverDropsStatsView.tsx
@@ -0,0 +1,39 @@
+import { KeyGiverDropsStatsTimeOptions } from "../../types/KeyGiverDrop";
+import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
+import { useSelect } from "../../components/Inputs/Select/useSelect";
+import { KeyGiverDropsStatsInfo } from "../../components/KeyGiverDropsStatsInfo/KeyGiverDropsStatsInfo";
+import { useKeyGiverDropsStats } from "../../hooks/useKeyGiverDropsStats";
+import classes from "./KeyGiverDropsStatsView.module.css";
+import { MobileWrapper } from "../../components/MobileWrapper/MobileWrapper";
+import { options } from "../../components/KeyGiverDropsStatsInfo/dataKeyGiverDropsStats";
+import { useEffect } from "react";
+
+dayjs.extend(timezone);
+
+export const KeyGiverDropsStatsView = () => {
+  const { value, Select } = useSelect(KeyGiverDropsStatsTimeOptions.last10days);
+  const { keyGiverDropsStats, isKeyGiverDropsStatsLoading } =
+    useKeyGiverDropsStats(
+      value as KeyGiverDropsStatsTimeOptions,
+      dayjs.tz.guess()
+    );
+
+  useEffect(() => {
+    console.table(keyGiverDropsStats);
+  }, [keyGiverDropsStats]);
+
+  return (
+    <div className={classes.KeyGiverDropsStatsView}>
+      <MobileWrapper>
+        <Select
+          placeholder="Wyświetl statystyki dropów za:"
+          options={options}
+          icon="calendar"
+          className={classes.select}
+        />
+      </MobileWrapper>
+      <KeyGiverDropsStatsInfo />
+    </div>
+  );
+};
