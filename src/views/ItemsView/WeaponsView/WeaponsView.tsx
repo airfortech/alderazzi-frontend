4b87669ab79624d5cd09af4b9344849bd1024b63
airfortech <airm@xxx.yy>
2024-03-23 20:41:44 +0100

    added all views for weapons

--- src/views/ItemsView/WeaponsView/WeaponsView.tsx
+++ src/views/ItemsView/WeaponsView/WeaponsView.tsx
@@ -1,10 +1,11 @@
 import { WeaponsList } from "../../../components/ItemsList/WeaponsList/WeaponsList";
+import { WeaponsListOption } from "../../../types/ItemsList";
 import classes from "../ItemsView.module.css";
 
-export const WeaponsView = () => {
+export const WeaponsView = (props: WeaponsListOption) => {
   return (
-    <div className={classes.ItemsWeaponsView}>
-      <WeaponsList />
+    <div key={props.path} className={classes.ItemsWeaponsView}>
+      <WeaponsList {...props} />
     </div>
   );
 };
